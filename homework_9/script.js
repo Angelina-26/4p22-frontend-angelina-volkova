bb9ec72eed91c60e16cf4faa41627fbbce72e1ca
Angelina <angelinabobrova@yandex.ru>
2022-10-27 13:11:46 +0400

    add homework_9

--- homework_9/script.js
+++ homework_9/script.js
@@ -3,7 +3,7 @@
 let firstArgument = document.querySelector('.form__input1');
 let secondArgument = document.querySelector('.form__input3');
 const operator = document.querySelector('.form__input2');
-let result = document.querySelector('.result');
+let form__result = document.querySelector('.form__result');
 const btn = document.querySelector('.form__button');
 
 btn.addEventListener('click', function () {
@@ -18,30 +18,27 @@ if (typeof val1 === 'number' && typeof val2 === 'number') {
         case "+":
             res = val1 + val2;
             console.log(res);
-            result.innerHTML = res;
+            form__result.innerHTML = res;
             break;
         case "-":
             res = val1 - val2;
             console.log(res);
-            result.innerHTML = res;
+            form__result.innerHTML = res;
             break;
         case "/":
             res = val1 / val2;
             console.log(res);
-            result.innerHTML = res;
+            form__result.innerHTML = res;
             break;
         case "*":
             res = val1 * val2;
             console.log(res);
-            result.innerHTML = res;
+            form__result.innerHTML = res;
             break;       
-
     }
 } else {
     console.log ('Некорректный ввод чисел')
-    alert ('Некорректный ввод чисел')
+    
 } 
-
-
 })
 
