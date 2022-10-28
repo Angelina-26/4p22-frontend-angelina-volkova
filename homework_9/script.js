3a7a14c3e6b5c3f712c87ea3fb3bf8a71c5aef07
Angelina <angelinabobrova@yandex.ru>
2022-10-28 12:35:30 +0400

    add homework_9

--- homework_9/script.js
+++ homework_9/script.js
@@ -10,6 +10,25 @@ btn.addEventListener('click', function () {
     const val1 = Number (firstArgument.value);
     const val2 = Number (secondArgument.value);
     let res;
+
+    event.preventDefault();
+
+    if (val1 == '') {
+        form__result.innerHTML = 'Первое число не указано';
+        return console.log('Первое число не указано');
+        
+    };
+
+    if (val2 == '') {
+        form__result.innerHTML = 'Второе число не указано';
+        return console.log('Второе число не указано');
+    };
+
+    if (isNaN(val1) === true || isNaN(val2) === true) {
+        form__result.innerHTML = 'Некорректный ввод чисел';
+        return console.log('Некорректный ввод чисел');
+    };
+
     console.log(val1, '.form__input1', secondArgument.value, val2, typeof val2);
 
 
@@ -34,11 +53,15 @@ if (typeof val1 === 'number' && typeof val2 === 'number') {
             res = val1 * val2;
             console.log(res);
             form__result.innerHTML = res;
-            break;       
-    }
-} else {
-    console.log ('Некорректный ввод чисел')
-    
+            break;   
+        case '':
+            console.log('Не введен знак');
+            form__result.innerHTML = 'Не введен знак'; 
+            break;  
+        default: return form__result.innerHTML = 'Программа не поддерживает такую операцию',
+        console.log('Программа не поддерживает такую операцию');
+        break;  
+    }    
 } 
 })
 
