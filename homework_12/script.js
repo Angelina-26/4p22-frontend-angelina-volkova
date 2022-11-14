5211b13deb4b484b1f811d2c604598289ef69731
Angelina <angelinabobrova@yandex.ru>
2022-11-14 15:47:45 +0400

    add homework_12

--- homework_12/script.js
+++ homework_12/script.js
@@ -1,13 +1,35 @@
 
-let firstArgument = document.querySelector('.reg__input');
-let reg__email = document.querySelector('reg__email');
+const input = document.querySelector('.reg__input');
 const btn = document.querySelector('.reg__button');
+const label = document.querySelector('.reg__email');
 
-btn.addEventListener('click', function (e) {
+btn.addEventListener('click', (e) => {
     e.preventDefault();
-    const val1_str = String(firstArgument.value);
-    if (val1_str.indexOf(' ')) {
-        return console.log('Поле обязательно для заполнения');
+    validateEmail(input.value);
+    if (validateEmail(input.value)) {
+        input.classList.add('green');
+        input.classList.remove('red');
+    } else {
+        input.classList.remove('green');
+        input.classList.add('red');
     }
-
 })
+
+btn.addEventListener('click' , (e) => {
+    e.preventDefault();
+    validateEmail(label.value);
+    if (validateEmail(label.value)) {
+        label.classList.add('green');
+        label.classList.remove('error');
+    } else {
+        label.classList.remove('green');
+        label.classList.add('error');
+    }
+})
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;    
+    console.log(re.test(String(email).toLowerCase()));
+    return re.test(String(email).toLowerCase());
+}
+
