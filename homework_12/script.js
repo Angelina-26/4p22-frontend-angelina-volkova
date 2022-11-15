e4157227754a47d6c769df8fa5bf47f46e4279f4
Angelina <angelinabobrova@yandex.ru>
2022-11-15 20:01:33 +0400

    add homework_12

--- homework_12/script.js
+++ homework_12/script.js
@@ -6,7 +6,7 @@ const error2 = document.querySelector('.error2');
 const input2 = document.querySelector('.reg__input2');
 const input3 = document.querySelector('.reg__input3');
 const error3 = document.querySelector('.error3');
-const error8 = document.querySelector('error8');
+const error8 = document.querySelector('.error8');
 
 
 btn.addEventListener('click', (e) => {
@@ -20,29 +20,32 @@ btn.addEventListener('click', (e) => {
         input.classList.add('red');
         error.classList.add('show');  
     }
-   
-    if (String(input2.value) != '') {
-        input2.classList.remove('red');
-        error2.classList.remove('show2');
-        
-    } else {
-        input2.classList.add('red');
-        error2.classList.add('show2');
-    }
 
-    if (String(input2.value) != (input3.value)) {
+    if (String(input3.value) != (input2.value)) {
         input3.classList.add('red');
         error3.classList.add('show3');
-    } 
 
-    /*if (String(input2.value).length<8) {
+    } else {
+        input2.classList.remove('red');
+        input2.classList.remove('show8');
+    }
+   
+    if (String(input2.value) === '') {
+        input2.classList.add('red');
+        error2.classList.add('show2');
+        
+    } else {
+        input2.classList.remove('red');
+        error2.classList.remove('show2');
+    }
+
+    if (input2.value.length < 8) {
         input2.classList.add('red');
         input2.classList.add('show8');
     } else {
         input2.classList.remove('red');
         input2.classList.remove('show8');
-
-    }*/
+    }
 })
 
 function validateEmail(email) {
