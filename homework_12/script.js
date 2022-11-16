ddcd9bb186eed2b5fbcb23ba73b846273c19f820
Angelina <angelinabobrova@yandex.ru>
2022-11-16 15:55:26 +0400

    add homework_12

--- homework_12/script.js
+++ homework_12/script.js
@@ -7,6 +7,9 @@ const input2 = document.querySelector('.reg__input2');
 const input3 = document.querySelector('.reg__input3');
 const error3 = document.querySelector('.error3');
 const error8 = document.querySelector('.error8');
+const about = document.querySelector('.reg__about-textarea');
+const gender = document.querySelector('.reg__radio-input');
+const subscribe = document.querySelector('.reg__checkbox-input');
 
 
 btn.addEventListener('click', (e) => {
@@ -21,30 +24,39 @@ btn.addEventListener('click', (e) => {
         error.classList.add('show');  
     }
 
-    if (String(input3.value) != (input2.value)) {
+    if (String(input3.value) !== (input2.value)) {
         input3.classList.add('red');
         error3.classList.add('show3');
 
     } else {
-        input2.classList.remove('red');
-        input2.classList.remove('show8');
+        input3.classList.remove('red');
+        error3.classList.remove('show3');
     }
    
-    if (String(input2.value) === '') {
+     if (String(input2.value) === '') {
         input2.classList.add('red');
         error2.classList.add('show2');
-        
+        error8.classList.remove('show8');
+    
+     } else if (input2.value.length < 8) {
+        input2.classList.add('red');
+        error8.classList.add('show8');
+        error2.classList.remove('show2');
+
     } else {
         input2.classList.remove('red');
         error2.classList.remove('show2');
+        error8.classList.remove('show8');
     }
-
-    if (input2.value.length < 8) {
-        input2.classList.add('red');
-        input2.classList.add('show8');
-    } else {
-        input2.classList.remove('red');
-        input2.classList.remove('show8');
+    if (!error2.classList.contains('show2') && !error.classList.contains('show') && !error3.classList.contains('show3') && !error8.classList.contains('show8')) {
+        const result = {
+            email: input.value,
+            password: input2.value,
+            about: about.value,
+            gender: gender.value,
+            subscribe: subscribe.value,
+        }
+        console.log('information' , result);
     }
 })
 
